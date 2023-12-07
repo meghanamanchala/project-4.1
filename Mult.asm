d001ad386dcbeb06893cb3b12b1c967573995547
Meghana <meghanamanchal@gmail.com>
2023-12-07 17:14:50 +0530

    added

---
+++ Mult.asm
@@ -0,0 +1,38 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+//MULTIPLICATION
+@100
+D=A
+@0
+M=D
+@3
+D=A
+@1
+M=D
+
+@3
+D=M
+D=D+1
+@3
+M=D
+@0
+D=M
+@2
+M=M+D
+
+@1
+D=M
+@3
+D=M-D
+@32767
+D;JEQ
+@8
+0;JMP
